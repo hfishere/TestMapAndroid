7d7d55beca892a9532620e959b975bc19fa5708a
Hafidz Fairiz <hafidzfairiz@gmail.com>
2019-01-22 01:20:24 +0700

    Add project files.

---
+++ TestMapAndroidOnly/MainActivity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Android.App;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace TestMapAndroidOnly
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        public void OnMapReady(GoogleMap map)
+        {
+            var latLong = new LatLng(-6.1858248, 106.6141176);
+            map.MapType = GoogleMap.MapTypeNormal;
+
+            MarkerOptions marker = new MarkerOptions();
+            marker.SetPosition(latLong);
+            marker.SetTitle("JNE Tangerang");
+
+            CameraPosition.Builder builder = CameraPosition.InvokeBuilder();
+            builder.Target(latLong);
+            builder.Zoom(18);
+            builder.Bearing(155);
+            builder.Tilt(65);
+
+            CameraPosition cameraPosition = builder.Build();
+
+            CameraUpdate cameraUpdate = CameraUpdateFactory.NewCameraPosition(cameraPosition);
+
+            map.MoveCamera(cameraUpdate);
+
+            map.AddMarker(marker);
+        }
+    }
+}
+
